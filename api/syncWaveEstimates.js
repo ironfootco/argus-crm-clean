241ba09a033927f97d68dad1878ed0acdc5479bd
ironfootco <ironfootco@gmail.com>
2026-08-09 21:58:20 -0400

    Replace 'summary' with 'memo' in syncWaveEstimates

--- api/syncWaveEstimates.js
+++ api/syncWaveEstimates.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
                     status
                     createdAt
                     title
-                    summary
+                    memo
                     customer {
                       id
                       name
