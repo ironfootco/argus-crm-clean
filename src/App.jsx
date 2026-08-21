c04f6a2cf928c8e086c47678eca947a86c0be2be
ironfootco <ironfootco@gmail.com>
2026-08-20 23:13:22 -0400

    Add CrewPortal component and route
    Added import for CrewPortal and new route for it.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import AllJobs from './pages/AllJobs';
 import ManagerHub from './pages/ManagerHub';
 import DesignSandbox from './pages/DesignSandbox';
 import Login from './components/Login';
+import CrewPortal from './CrewPortal'; // 🆕 IMPORT ADDED HERE
 
 /* ========================================================================== */
 /* 🔓 PUBLIC BOOKING COMPONENT                                                */
@@ -1692,6 +1693,9 @@ export default function App() {
                     activeWorker === 'Edwin' ? <Navigate to="/" replace /> : <ManagerHub />
                   } 
                 />
+
+                {/* 🆕 NEW CREW PORTAL ROUTE ADDED HERE */}
+                <Route path="/crew" element={<CrewPortal />} />
 
                 <Route path="/sandbox" element={<DesignSandbox />} />
                 <Route path="/privacy" element={<PrivacyPolicy />} />
