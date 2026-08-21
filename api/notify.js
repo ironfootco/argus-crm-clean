d7217b9aefd96bf062da9a8a1190c143af766916
ironfootco <ironfootco@gmail.com>
2026-08-21 18:18:22 -0400

    Change Authorization method to Basic for OneSignal

--- api/notify.js
+++ api/notify.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     headers: {
       accept: 'application/json',
       'content-type': 'application/json',
-      Authorization: `Key ${process.env.ONESIGNAL_REST_API_KEY}`
+      Authorization: `Basic ${process.env.ONESIGNAL_REST_API_KEY}`
     },
     body: JSON.stringify({
       app_id: 'e07ab1fb-c308-4bc7-9555-6172a01ac793',
@@ -21,6 +21,12 @@ export default async function handler(req, res) {
   try {
     const response = await fetch('https://onesignal.com/api/v1/notifications', options);
     const data = await response.json();
+
+    if (!response.ok) {
+      console.error('OneSignal API Error:', data);
+      return res.status(response.status).json(data);
+    }
+
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: 'Error sending notification' });
